9536d788870ce2f0d92bef9a914a618cbe78a08c
Kettengeschwindigkeit <666damned999@mail.ru>
2022-08-20 17:52:10 +0300

    initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const Header = () => <h1>Header</h1>
+const MainContent = () => <h1>Main Content</h1>
+const Footer = () => <h1>Footer</h1>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  )
 }
 
 export default App;
